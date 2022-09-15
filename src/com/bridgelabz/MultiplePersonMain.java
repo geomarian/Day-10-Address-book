8499bc93fb4596fb9afe91915c2a2a0f96661da7
geomarian <geomarian520@gmail.com>
2022-09-16 01:29:16 +0530

    Main Branch added

---
+++ src/com/bridgelabz/MultiplePersonMain.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package com.bridgelabz;
+
+/**
+ * @author ASUS
+ *
+ */
+public class MultiplePersonMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//UC 5
+		MultiplePersonUC5 multiplePerson = new MultiplePersonUC5();
+		multiplePerson.userDetails();
+		//multiplePerson.userDetails();
+		
+		
+		//UC6
+		MultipleAddressBookUC6 multipleAddressBook = new MultipleAddressBookUC6();
+		multipleAddressBook.addressBook();
+	}
+
+}
